9bcb9291feadbb353dc79ff2d0df5abd98f0fe91
Kim-yujin <w2117@e-mirim.hs.kr>
2023-03-04 20:57:44 +0900

    서블릿 코드 작성

--- src/main/java/org/zerock/webfromjava/MyServlet.java
+++ src/main/java/org/zerock/webfromjava/MyServlet.java
@@ -1,4 +1,24 @@
 package org.zerock.webfromjava;
 
-public class MyServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//WebServlet 어노테이션을 이용해서 해당 서블릿의 경로 지정
+@WebServlet(name = "myServlet", urlPatterns = "/my")
+//HttpServlet 상속
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>MyServlet</hr>");
+        out.println("</body></html>");
+    }
 }
